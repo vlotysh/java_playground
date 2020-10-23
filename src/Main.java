6e4af1d68dd404833d5200c4c9eaa34fd9100889
vlad lotysh <gangsta1@i.ua>
2020-10-23 18:50:55 +0300

    Added threading

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,28 @@
 //import math.AreaOfTriangle;
 
 import sort.MergeSort;
+import threading.ThreadVersion;
 
 import java.io.IOException;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
+        startMultiThread();
+    }
+
+    private static void startMultiThread()
+    {
+        ThreadVersion T1 = new ThreadVersion( "Thread-1");
+        T1.start();
+
+        ThreadVersion T2 = new ThreadVersion( "Thread-2");
+        T2.start();
+    }
+
+    private static void runArraySort () throws IOException
+    {
+        System.out.println(System.getProperty("java.version"));
         int[] arrayToSort = (new InputArray()).input();
         printArray(arrayToSort);
 
@@ -16,7 +32,8 @@ public class Main {
         printArray(result);
     }
 
-    public static void printArray(int[] array) {
+
+    private static void printArray(int[] array) {
         Arrays.stream(array).mapToObj(j -> j + "  ").forEach(System.out::print);
     }
 }
